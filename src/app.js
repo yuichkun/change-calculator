ca8b57a6e43ad710ee6d925fca47cb5e252b7764
Yuichi Yogo <yuichkun@gmail.com>
2020-09-17 00:06:07 +0900

    impl determineChangeDistribution

--- src/app.js
+++ src/app.js
@@ -3,14 +3,27 @@ console.log("app started")
 const COIN_TYPES = [500, 100, 50, 10, 5, 1]
 
 function determineChangeDistribution(changeTotalAmount) {
-  return {
+  let rest = changeTotalAmount
+
+  let distribution = {
     500: 0,
-    100: 4,
+    100: 0,
     50: 0,
     10: 0,
     5: 0,
     1: 0
   }
+
+  for (let i = 0; i < COIN_TYPES.length; i++){
+    const coinType = COIN_TYPES[i]
+    const quotient = Math.floor(rest / coinType)
+    if (quotient > 0) {
+      distribution[coinType] = quotient
+      rest -= quotient * coinType
+    }
+  }
+
+  return distribution
 }
 
 module.exports = {
--- src/app.test.js
+++ src/app.test.js
@@ -12,5 +12,27 @@ describe("app", () => {
         1: 0
       })
     })
+    it("returns 500yen * 1, 50yen * 1, 1yen * 1 when change total amount is 551", () => {
+      const changeDistribution = determineChangeDistribution(551)
+      expect(changeDistribution).toEqual({
+        500: 1,
+        100: 0,
+        50: 1,
+        10: 0,
+        5: 0,
+        1: 1
+      })
+    })
+    it("returns 500yen * 3, 100yen * 3, 50yen * 1, 10yen * 2, 5yen * 1, 1yen * 1 when change total amount is 1876", () => {
+      const changeDistribution = determineChangeDistribution(1876)
+      expect(changeDistribution).toEqual({
+        500: 3,
+        100: 3,
+        50: 1,
+        10: 2,
+        5: 1,
+        1: 1
+      })
+    })
   })
 })
